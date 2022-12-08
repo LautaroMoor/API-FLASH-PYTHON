fbaeb420de8dbada5c9d92346628ae96b41b73f9
Lautaro Moor <lautimoor2@gmail.com>
2022-12-08 01:09:12 -0300

    Ultimas diez peliculas

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ from os import system
 
 app = Flask(__name__)
 
+#Leer JSONs
 with open ('jsons/usuarios.json','r') as j:
     usuarios = json.load(j)
+
+with open ('jsons/peliculas.json','r') as j:
+    peliculas = json.load(j)
+
+with open ('jsons/directores.json','r') as j:
+    directores = json.load(j)
+
+with open ('jsons/generos.json','r') as j:
+    generos = json.load(j)
 
 def MenuBienvenida():
     opcion = 0
@@ -17,8 +27,7 @@ def MenuBienvenida():
         print('2) Ingresar como invitado')
         print('3) Para salir')
         print('=====================')
-        print('Ingresar opcion: ')
-        opcion = int(input('Ingresar: '))
+        opcion = int(input('Ingresar opcion: '))
     return opcion
 
 def opcionIniciarSesion():
@@ -33,13 +42,61 @@ def opcionIniciarSesion():
             if usuario["usuario"] == usuarioIngresado and usuario["contrasena"] == contrasenaIngresada:
                 input('Logeo exitoso!! Enter para continuar!')
                 inicioExitoso = True
-                return inicioExitoso 
-            
+        if inicioExitoso:
+            break
+
+def menuUsuario():
+    opcion = 0
+    while not(opcion>=1 and opcion<=4):
+        system("cls")
+        print('=====================')
+        print('1) Ver ultimas 10 peliculas agregadas')
+        print('2) Agregar pelicula')
+        print('2) Modificar pelicula')
+        print('3) Borrar pelicula')
+        print('4) Buscar pelicula por ID')
+        print('=====================')
+        opcion = int(input('Ingrese opcion: '))
+    return opcion
+
+def menuInvitado():
+    opcion = 0
+    while not(opcion == 1):
+        system("cls")
+        print('=====================')
+        print('1) Ver ultimas 10 peliculas agregadas')
+        print('=====================')
+        opcion = int(input('Ingrese opcion: '))
+    return opcion
+
+def ultimasDiezPeliculas():
+    contador = 0
+    system("cls")
+    for pelicula in reversed(peliculas):
+        contador = contador + 1
+        print(f'ID {pelicula["id"]} = La pelicula {pelicula["titulo"]} salio en el año {pelicula["ano"]}, el director fue {directores[pelicula["idDirector"]]["nombre"]}, ', end='')
+        print('tiene los generos ', end="")
+        for generoPelis in pelicula["idGeneros"]:
+            for genero in generos:
+                if genero["id"] == generoPelis:
+                    print(genero["nombre"].lower(),', ', end="")
+        print('La sinopsis es:', pelicula["sinopsis"])
+        if contador == 10:
+            break
+    input('Ingrese enter para continuar...')
+
 def main():
     opcionMenuBienvennida = 0
     while opcionMenuBienvennida != 3:
         opcionMenuBienvennida = MenuBienvenida()
         if opcionMenuBienvennida == 1:
             opcionIniciarSesion()
+            opcion = menuUsuario()
+            if opcion == 1:
+                ultimasDiezPeliculas()
+        if opcionMenuBienvennida == 2:
+            opcion = menuInvitado()
+            if opcion == 1:
+                ultimasDiezPeliculas()
 
 main()
